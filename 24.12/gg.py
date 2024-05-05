28cda81795ce0db045cccdc6c23a6aef3b850d32
footblastnik <127666692+footblastnik@users.noreply.github.com>
2024-05-05 18:26:55 +0700

    Projekt(Nikita2024
    weeeeeeeee

---
+++ 24.12/gg.py
@@ -0,0 +1,49 @@
+# name = input("Введите имя: ")
+# sur_name = input("Введите фамилию: ")
+# 
+# while name != "Неждан" or sur_name != "Мохноногов":
+#     print("Нет, я жду Неждана Мохноногова, тебя не пущу!")
+#     print("А ты кто?")
+#     name = input("Введите имя: ")
+#     sur_name = input("Введите фамилию: ")
+# 
+# print("Проходи, Нежданчик!")
+# 
+# otvet = input("Отгадай загадку: зимой и летом одним цветом: ")
+# 
+# while otvet != "человек-паук":
+#     if otvet == "ель":
+#         print("Неплохо, но не угадал")
+#     elif otvet == "небо":
+#         print("Да не, небо разного цвета даже за один день")
+#     else:
+#         print("Нет, не верно")
+#     otvet = input("Отгадай загадку: зимой и летом одним цветом: ")
+# 
+# print("Верно! Паутина из рук и костюм один и тот же")
+
+# name = input("Введите пороль: ")
+# while name != "dimaw":
+#       print("пороль не верин ")
+#       name = input("Введите пороль: ")
+# print("пороль верин!")
+# )_____________________________________________________2
+# name = input("логин: ")
+# sur_name = input("Введите пороль: ")
+# while name != "dimapopkov" or sur_name != "popkovdima":
+#      print("Не верно ")
+#      print("введите логин и пароль")
+#      name = input("Введите логин: ")
+#      sur_name = input("Введите пороль: ") 
+# print("успешно")
+
+name = input("логин: ")
+sur_name = input("Введите пороль: ")
+while name != "dimapopkov" or sur_name != "popkovdima":
+      print("Не верно ")
+      if name != "dimapopkov":
+          print("логин не верин")
+      if sur_name != "popkovdima":
+          print("пороль не верин")
+      name = input("Введите логин: ") 
+print("успешно")
